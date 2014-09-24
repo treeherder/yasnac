646ded0c8fb1e9c61a1148c69df7894a36bd2c58
jerkey <jerkey@g-cipher.net>
2014-09-23 23:38:59 -0700

    filename change and major clean-up

--- src/fakedisk.py
+++ src/fakedisk.py
@@ -1,15 +1,12 @@
-import serial, time, sys
-
+import serial, time
 
 class YASNAC():  # a classs to handle the yasnac
   def __init__(self):
     self.com = serial.Serial(port='/dev/ttyS0', baudrate=4800,parity=serial.PARITY_EVEN,timeout=0.5)
-    #initialize the class a connection to the serial port
     time.sleep(1)# wait for the port to be ready (this is an arbitrary period)
-    sys.stdout.write('opened serial port.\n')
-    sys.stdout.flush()
+    print('opened serial port.')
 
-  def rx(self, cue):
+  def rx(self):
     packet = ''
     while self.com.inWaiting():
       r = self.com.read()
@@ -37,29 +34,10 @@ class YASNAC():  # a classs to handle the yasnac
     packet = '\x02'+response+chr(checksum % 256)+chr(checksum / 256)
     self.com.write(packet)
   
-  #rx() and tx() are the two most basic methods of this class
-  #handshake is an example of combining rx() and tx() 
-  
-  def handshake(self): # press flesh with the yasnac
-    if(self.rx("ENQ")  == True):  #default value for handshake
-      self.tx(None) #default value for handshake 
-    else:
-      print("handshake failed, no inquiry heard")    
-    return(self.rx(None))  #ready for the next step
-
-  def list_files(self, filenames): #filenames as a single string in ASCII separated by FOUR (4) spaces
-    # self.tx("0273004C5354{0}1FE0".format(filenames.encode("hex")).decode("hex")) # "\x02\x73\x00LST0009123\x2e4C5354{0}1FE0"
-    response = "\x02\x13\x00LST{0}\x00\xfc".format(filenames)
-    checksum = sum([ord(c) for c in response])
-    print response+" "+str(checksum) # checksum is 1278, any change results in NAK
-    self.tx(response) #.format(filenames.encode("hex")).decode("hex")) # "\x02\x73\x00LST0009123\x2e4C5354{0}1FE0"
-    return
-        
-# some procedural style stuff to get you started:
 moto = YASNAC()  #instantiate the class
 filename = '123.JBI'
 while True:
-  packet = moto.rx(None)
+  packet = moto.rx()
   if packet != '':
     print packet
   if "ENQ" in packet:
@@ -77,4 +55,4 @@ while True:
   elif "DSZ" in packet:
     print "replying with DSZ00729088"
     time.sleep(0.005)
-    moto.tx("DSZ00729088")
+    moto.tx("DSZ00729088") # tell it how much free space on our "disk"
